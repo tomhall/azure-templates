d611277aa49536340b23cb21a515ee7ce6020565
Tom Hall <tom.hall@halltom.net>
2016-09-19 22:19:04 +1000

    Updating Octopus DSC to use version with TLS support (https://github.com/OctopusDeploy/OctopusDSC/pull/17)

--- Install-OctopusDSC.ps1
+++ Install-OctopusDSC.ps1
@@ -1,7 +1,7 @@
 if (-not (Test-Path "C:\Program Files\WindowsPowerShell\Modules\OctopusDSC")) {
     mkdir c:\temp -ErrorAction SilentlyContinue | Out-Null
     $client = new-object system.Net.Webclient
-    $client.DownloadFile("https://github.com/OctopusDeploy/OctopusDSC/archive/master.zip","c:\temp\octopusdsc.zip")
+    $client.DownloadFile("https://github.com/OctopusDeploy/OctopusDSC/archive/bug-tls12.zip","c:\temp\octopusdsc.zip")
     Add-Type -AssemblyName System.IO.Compression.FileSystem
     [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\temp\octopusdsc.zip", "c:\temp")
     cp -Recurse C:\temp\OctopusDSC-master\OctopusDSC "C:\Program Files\WindowsPowerShell\Modules\OctopusDSC"
