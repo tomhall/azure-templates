3ecde7b4fd71eba703503cd53c329bd2120be220
Tom Hall <tom.hall@halltom.net>
2016-09-26 23:18:09 +1000

    New Relic Installation.

--- Install-NewRelic.ps1
+++ Install-NewRelic.ps1
@@ -4,6 +4,17 @@ param (
     [String]$NewRelicLicenseKey
 )
 
+function downloadFile($uri, $outfileName) {
+	Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $outfileName
+	return Resolve-Path $outfileName
+}
+
 # Install New Relic Server Agent
+$fileName = "NewRelicServerMonitor_x64_3.3.6.0.msi"
+$filePath = downloadFile "https://download.newrelic.com/windows_server_monitor/release/NewRelicServerMonitor_x64_3.3.6.0.msi" $fileName
+Start-Process "msiexec" -ArgumentList "/i", "$filePath", "/L*v", "install.log", "/qn", "NR_LICENSE_KEY=$NewRelicLicenseKey"
 
 # Install New Relic Dot Net Agent
+$fileName = "NewRelicDotNetAgent_x64.msi"
+$filePath = downloadFile http://download.newrelic.com/dot_net_agent/release/NewRelicDotNetAgent_x64.msi $fileName
+Start-Process "msiexec.exe" -ArgumentList "/i", "$filePath", "/qb", "NR_LICENSE_KEY=$NewRelicLicenseKey", 'INSTALLLEVEL=1'
