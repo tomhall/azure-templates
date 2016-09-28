42bca17843d1923a7d5992d00c104a360dcc2f2e
Tom Hall <tom.hall@halltom.net>
2016-09-29 08:13:53 +1000

    Adding IIS URLRewrite Module to the WebServerConfig Script.

--- WebServerConfig.ps1
+++ WebServerConfig.ps1
@@ -48,6 +48,18 @@ Configuration WebServerConfig
 			Ensure = "Present"
 		}
 
+		#Install URL Rewrite module for IIS
+		Package UrlRewrite
+		{
+			DependsOn = "[WindowsFeaturesWebServer]windowsFeatures"
+			Ensure = "Present"
+			Name = "IIS URL Rewrite Module 2"
+			Path = "http://download.microsoft.com/download/6/7/D/67D80164-7DD0-48AF-86E3-DE7A182D6815/rewrite_2.0_rtw_x64.msi"
+			Arguments = "/quiet"
+			ProductId = "EB675D0A-2C95-405B-BEE8-B42A65D23E11"
+		}
+		
+		#Install Octopus Deploy Tentacle
 		cTentacleAgent OctopusTentacle 
         { 
             Ensure = "Present"; 
