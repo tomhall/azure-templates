0c0be695ec7bac0a28d8c1d2e33967ba582da4f1
Tom Hall <tom.hall@halltom.net>
2016-09-29 09:26:16 +1000

    Correcting URLRewrite Module Dependency on IIS.

--- WebServerConfig.ps1
+++ WebServerConfig.ps1
@@ -51,7 +51,7 @@ Configuration WebServerConfig
 		#Install URL Rewrite module for IIS
 		Package UrlRewrite
 		{
-			DependsOn = "[WindowsFeaturesWebServer]windowsFeatures"
+			DependsOn = "[WindowsFeature]IIS"
 			Ensure = "Present"
 			Name = "IIS URL Rewrite Module 2"
 			Path = "http://download.microsoft.com/download/6/7/D/67D80164-7DD0-48AF-86E3-DE7A182D6815/rewrite_2.0_rtw_x64.msi"
