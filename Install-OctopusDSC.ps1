45ea802402921787567f1a8bc3051601a5bf1b88
Tom Hall <tom.hall@ofx.com>
2018-03-02 11:41:42 +1100

    Use `raw.github.com` instead of `github.com` due to SSL/TLS errors when downloading files from Github.

--- Install-OctopusDSC.ps1
+++ Install-OctopusDSC.ps1
@@ -1,7 +1,7 @@
 if (-not (Test-Path "C:\Program Files\WindowsPowerShell\Modules\OctopusDSC")) {
     mkdir c:\temp -ErrorAction SilentlyContinue | Out-Null
     $client = new-object system.Net.Webclient
-    $client.DownloadFile("https://github.com/tomhall/azure-templates/raw/master/OctopusDSC.zip","c:\temp\octopusdsc.zip")
+    $client.DownloadFile("https://raw.github.com/tomhall/azure-templates/master/OctopusDSC.zip","c:\temp\octopusdsc.zip")
     Add-Type -AssemblyName System.IO.Compression.FileSystem
     [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\temp\octopusdsc.zip", "c:\temp")
     cp -Recurse C:\temp\OctopusDSC\OctopusDSC "C:\Program Files\WindowsPowerShell\Modules\OctopusDSC"
