a48ab712a5a6890a815573ab59f14a99d84c979f
Tom Hall <tom.hall@ofx.com>
2018-03-02 16:31:29 +1100

    Changing the installation order of web server prerequisites.

--- Install-WebServerPrerequisites.ps1
+++ Install-WebServerPrerequisites.ps1
@@ -4,11 +4,11 @@ param (
     [String]$NewRelicLicenseKey
 )
 
-Write-Host "Installing Chocolatey..."
-.\install.ps1
-
-Write-Host "Installing New Relic Agents"
-.\Install-NewRelic.ps1 -NewRelicLicenseKey $NewRelicLicenseKey
-
 Write-Host "Installing Octopus Deploy DSC Extension..."
 .\Install-OctopusDSC.ps1
+
+Write-Host "Installing New Relic Agents..."
+.\Install-NewRelic.ps1 -NewRelicLicenseKey $NewRelicLicenseKey
+
+Write-Host "Installing Chocolatey..."
+.\install.ps1
